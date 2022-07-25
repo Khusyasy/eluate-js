e628008f870aa84c45f891ac10ea71e00aaaffcc
Muhammad Luthfi Khusyasy <khusyasy@gmail.com>
2022-07-25 16:38:33 +0700

    change recursive style

--- src/core/initElement.js
+++ src/core/initElement.js
@@ -7,13 +7,17 @@ function initElement(element, proxy) {
   Object.entries(element.dataset).forEach(([name, value]) => {
     if (name === 'text') {
       textImpl(element, value, proxy);
+      delete element.dataset[name];
     } else if (name.startsWith('on:')) {
       const [, eventName] = name.split(':');
       eventImpl(eventName, element, value, proxy);
+      delete element.dataset[name];
     } else if (name === 'show') {
       showImpl(element, value, proxy);
+      delete element.dataset[name];
     } else if (name === 'model') {
       modelImpl(element, value, proxy);
+      delete element.dataset[name];
     } else if (name === 'set') {
       throw new Error('`data-set` cannot be inside another `data-set`');
     }
--- src/core/initRecursive.js
+++ src/core/initRecursive.js
@@ -2,19 +2,14 @@ import initRoot from './initRoot';
 
 // find element with 'data-set' to initialize the root element of the app
 function initRecursive(element) {
-  Object.entries(element.dataset).forEach(([name, value]) => {
-    if (name === 'set') {
-      initRoot(element, value);
-    }
-  });
-  [...element.children].forEach((child) => {
-    initRecursive(child);
-  });
-  // remove all 'data-*' attributes
-  // idk, just to make the html looks cleaner
-    Object.entries(element.dataset).forEach(([name, _]) => {
-    delete element.dataset[name];
-  });
+  if (element.dataset['set']) {
+    initRoot(element, element.dataset['set']);
+  } else {
+    [...element.children].forEach((child) => {
+      initRecursive(child);
+    });
+  }
+  delete element.dataset['set'];
 }
 
 export default initRecursive;
