9af1a0ca1e4b2ca3937bd3a7d21b3a796adab8c5
Muhammad Luthfi Khusyasy <khusyasy@gmail.com>
2022-07-25 22:32:32 +0700

    combine specials prop

--- src/core/createObjectProxy.js
+++ src/core/createObjectProxy.js
@@ -1,61 +1,52 @@
 function createObjectProxy(target = {}) {
-  // special property used
-  const $text = [];
-  const $show = [];
-  const $model = [];
-  function $get(prop) {
-    const setGlobal = Object.keys(target)
-      .map((k) => `globalThis.${k} = this.${k}`)
-      .join(';');
-    return new Function(`${setGlobal};\nreturn ${prop}`).call(target);
-  }
-  function $set(prop, value) {
-    // user JSON stringify to any value convert into string
-    let valueString = JSON.stringify(value);
-    new Function(`return this.${prop} = ${valueString}`).call(target);
+  // special properties used
+  const $specials = {
+    $text: [],
+    $show: [],
+    $model: [],
+    $get(prop) {
+      const setGlobal = Object.keys(target)
+        .map((k) => `globalThis.${k} = this.${k}`)
+        .join(';');
+      return new Function(`${setGlobal};\nreturn ${prop}`).call(target);
+    },
+    $set(prop, value) {
+      // user JSON stringify to any value convert into string
+      let valueString = JSON.stringify(value);
+      new Function(`return this.${prop} = ${valueString}`).call(target);
 
-    // update the real DOM
-    // data-text
-    $text.forEach(({ element, value }) => {
-      element.innerHTML = proxy.$get(value);
-    });
-    // data-show
-    $show.forEach(({ element, value }) => {
-      element.style.display = proxy.$get(value) ? '' : 'none';
-    });
-    // data-model
-    $model.forEach(({ updateFn }) => {
-      updateFn();
-    });
-  }
+      // update the real DOM
+      // data-text
+      this.$text.forEach(({ element, value }) => {
+        element.innerHTML = proxy.$get(value);
+      });
+      // data-show
+      this.$show.forEach(({ element, value }) => {
+        element.style.display = proxy.$get(value) ? '' : 'none';
+      });
+      // data-model
+      this.$model.forEach(({ updateFn }) => {
+        updateFn();
+      });
+    },
+  };
 
-  // TODO: check for cases like nested objects or arrays for data-text and data-show (and maybe more later)
   const proxy = new Proxy(target, {
     get(target, prop) {
       if (prop.startsWith('$')) {
-        if (prop === '$text') {
-          return $text;
-        } else if (prop === '$show') {
-          return $show;
-        } else if (prop === '$model') {
-          return $model;
-        } else if (prop === '$get') {
-          return $get;
-        } else if (prop === '$set') {
-          return $set;
-        } else {
-          throw new Error(`Unknown special property: ${prop}`);
-        }
+        const $special = $specials[prop];
+        if (!$special) throw new Error(`unknown special property '${prop}'`);
+        return $special;
       }
 
-      return $get(prop);
+      return $specials.$get(prop);
     },
     set(target, prop, value) {
       if (prop.startsWith('$')) {
         return "can't set special property";
       }
 
-      $set(prop, value);
+      $specials.$set(prop, value);
     },
   });
   return proxy;
